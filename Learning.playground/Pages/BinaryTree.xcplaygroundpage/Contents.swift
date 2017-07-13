9b0c54a6faa963f68fb2af49d16d7b7536eb02ca
Toan.Quach <toan.quach@sutrixmedia.com>
2017-07-13 17:43:10 +0700

    BinaryTree

---
+++ Learning.playground/Pages/BinaryTree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+//
+// from Objc.io subject Binary Tree
+//
+enum BinaryTree<Element: Comparable> {
+  case empty
+  indirect case node(Element, left: BinaryTree<Element>, right: BinaryTree<Element>)
+}
+
+extension BinaryTree {
+  func inserting(_ newElement: Element) -> BinaryTree<Element> {
+    switch self {
+    case .empty:
+      return .node(newElement, left: .empty, right: .empty)
+      
+    case let .node(element, left, right):
+      if element < newElement {
+        return .node(element, left: left, right: right.inserting(newElement))
+      } else {
+        return .node(element, left: left.inserting(newElement), right: right)
+      }
+    }
+  }
+  
+//  mutating func insert(_ newElement: Element) {
+//    self = inserting(newElement)
+//  }
+  
+  func contains(_ element: Element) -> Bool {
+    switch self {
+    case .empty:
+      return false
+    case let .node(member, left, right):
+      if member == element {
+        return true
+      } else if member < element {
+        return right.contains(element)
+      } else {
+        return left.contains(element)
+      }
+    }
+  }
+}
+
+extension BinaryTree: CustomStringConvertible {
+  var description: String {
+    switch self {
+    case .empty:
+      return "()"
+    case let .node(element, .empty, .empty):
+      return "(\(element))"
+    case let .node(element, left, right):
+      return "(\(left) \(element) \(right))"
+    }
+  }
+}
+
+let tree = BinaryTree<Int>.empty
+let t = tree.inserting(3).inserting(5).inserting(2).inserting(10).inserting(7)
+
+t.contains(2)
+t.contains(11)
